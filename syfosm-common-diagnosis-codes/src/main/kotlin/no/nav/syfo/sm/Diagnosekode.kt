d9347b07ae904fb2c2ed1db3260c3ba0275667a4
Joakim Kartveit <joakimkartveit@gmail.com>
2019-11-01 08:20:52 +0100

    Legger til DeserializationFeature

--- syfosm-common-diagnosis-codes/src/main/kotlin/no/nav/syfo/sm/Diagnosekode.kt
+++ syfosm-common-diagnosis-codes/src/main/kotlin/no/nav/syfo/sm/Diagnosekode.kt
@@ -1,5 +1,6 @@
 package no.nav.syfo.sm
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
