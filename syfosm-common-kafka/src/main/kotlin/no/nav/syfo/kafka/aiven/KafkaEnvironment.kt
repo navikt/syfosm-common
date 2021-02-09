f4672d665c075582dc151d1da842f8cdd62bea79
andreasnilsen <danduras@gmail.com>
2021-02-09 14:38:33 +0100

    Add configs for aiven kafka

---
+++ syfosm-common-kafka/src/main/kotlin/no/nav/syfo/kafka/aiven/KafkaEnvironment.kt
@@ -0,0 +1,18 @@
+package no.nav.syfo.kafka.aiven
+
+data class KafkaEnvironment(
+    val KAFKA_BROKERS: String = getEnvVar("KAFKA_BROKERS"),
+    val KAFKA_CERTIFICATE_PATH: String = getEnvVar("KAFKA_CERTIFICATE_PATH"),
+    val KAFKA_PRIVATE_KEY_PATH: String = getEnvVar("KAFKA_PRIVATE_KEY_PATH"),
+    val KAFKA_CA_PATH: String = getEnvVar("KAFKA_CA_PATH"),
+    val KAFKA_CLIENT_ID: String = getEnvVar("KAFKA_CLIENT_ID"),
+    val KAFKA_TRUSTSTORE_PATH: String = getEnvVar("KAFKA_TRUSTSTORE_PATH"),
+    val KAFKA_KEYSTORE_PATH: String = getEnvVar("KAFKA_KEYSTORE_PATH"),
+    val KAFKA_CREDSTORE_PASSWORD: String = getEnvVar("KAFKA_CREDSTORE_PASSWORD")
+
+) {
+    companion object {
+        fun getEnvVar(varName: String, defaultValue: String? = null) =
+            System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
+    }
+}
