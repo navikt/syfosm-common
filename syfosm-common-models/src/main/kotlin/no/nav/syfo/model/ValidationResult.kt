bb834473a1a07807fffef494dcafa0782aff805c
Mathias Hellevang <mathias.hellevang@gmail.com>
2022-02-01 14:27:04 +0100

    Fjern juridisk vurdering fra validationresult

--- syfosm-common-models/src/main/kotlin/no/nav/syfo/model/ValidationResult.kt
+++ syfosm-common-models/src/main/kotlin/no/nav/syfo/model/ValidationResult.kt
@@ -1,11 +1,8 @@
 package no.nav.syfo.model
 
-import no.nav.syfo.model.juridisk.JuridiskVurdering
-
 data class ValidationResult(
     val status: Status,
-    val ruleHits: List<RuleInfo>,
-    val juridiskeVurderinger: List<JuridiskVurdering>?
+    val ruleHits: List<RuleInfo>
 )
 
 data class RuleInfo(
